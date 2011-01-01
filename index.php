1f82366a7ba0b31f9707d34cb051cee798cfc3c4
Dan Beam <dan@danbeam.org>
2010-12-31 17:32:58 -0800

    more aliases and cache dir and .htaccess caching

--- index.php
+++ index.php
@@ -7,32 +7,39 @@
     <head>
         <title>How Many Googles?</title>
         <style type="text/css">
+            .body {
+                background-color: #ddd;
+            }
             .outer {
                 text-align: center;
                 top: 50%;
                 left: 50%;
-                margin: -110px 0 0 -323px;
+                margin: -153px -3px -3px -394px;
+                width: 782px;
                 position: absolute;
+                background-color: #eee;
+                border: 3px solid #ccc;
+            }
+            .rounded {
+                border-radius: 10px;
+                -moz-border-radius: 10px;
+                -webkit-border-radius: 10px;
             }
             h1, form, input, span, select, option {
                 font-size: 36px;
                 line-height: 40px;
                 padding: 2px;
                 font-family: "trebuchet ms", arial, helvetica, sans-serif;
+                background-color: transparent;
             }
-            select {
+            select, option {
                 border-width: 0;
                 padding: 0;
-                border-bottom: 2px solid #ccc;
-                background-color: #fff;
                 font-weight: bold;
-            }
-            form {
-                margin-bottom: 30px;
             }
             form input, form span {
                 border-width: 0;
-                border-bottom: 2px solid #ccc;
+                border-bottom: 4px solid #333;
                 display: inline-block;
                 width: 50px;
                 text-align: center;
@@ -41,31 +48,45 @@
                 zoom: 1;
                 display: inline;
             }
+            form {
+                margin-bottom: 29px;
+            }
             #submit {
+                color: #fff;
                 width: auto;
-                border: 2px solid #ccc;
-                padding: 2px 10px;
                 margin-left: 20px;
+                background-color: #666;
+                border: 2px solid #333;
+            }
+            #link, #submit {
+                padding: 2px 10px;
+            }
+            #link {
+                display: block;
+                margin-bottom: 29px;
             }
         </style>
         <script type="text/javascript">//<![CDATA[
             
+            // this is same as DOM ready
             window.onload = function () {
 
-                var numbs   = /[1-9][0-9]*/,
+                var numbs   = /[1-9][0-9]?/,
                     form    = document.getElementsByTagName('form')[0],
                     inputs  = form.getElementsByTagName('input'),
                     total   = form.getElementsByTagName('span')[0],
                     select  = form.getElementsByTagName('select')[0],
                     link    = document.getElementById('link');
 
-                inputs[0].onkeyup = inputs[1].onkeyup = function () {
+                form.onkeyup = function () {
                     if (numbs.test(inputs[0].value) && numbs.test(inputs[1].value)) {
                         total.innerHTML = parseInt(inputs[0].value, 10) * parseInt(inputs[1].value, 10);
                         total.style.display = '';
+                        link.style.display = '';
                     }
                     else {
                         total.style.display = 'none';
+                        link.style.display = 'none';
                     }
                 };
 
@@ -74,26 +95,25 @@
                     e.preventDefault && e.preventDefault();
                     e.returnValue = false;
                     if (numbs.test(inputs[0].value) && numbs.test(inputs[1].value)) {
-                        vanity = '/' + select.options[select.selectedIndex].value + '/' + inputs[0].value + 'x' + inputs[1].value;
-                        link.innerHTML = 'Link: ' + '<a href="' + vanity + '">' + vanity + '</a>';
+                        vanity = select.options[select.selectedIndex].value + '/' + inputs[0].value + 'x' + inputs[1].value;
+                        link.innerHTML = 'Link: ' + '<a href="/' + vanity + '">' + window.location.href + vanity + '</a>';
                     }
                     return false;
                 };
 
-                inputs[0].onkeyup();
+                form.onkeyup();
 
             };
 
         //]></script>
     </head>
-
-    <body>
-        <div class="outer">
-            <form action="google.php" method="get">
-                <h1>How many <select name="s"><option value="bings">Bings</option><option value="googles">Googles</option><option value="yahoos">Yahoos</option></select> would you like?</h1>
-                <input type="text" name="x" size="2" /> by <input type="text" name="y" size="2" /> = <span>:(</span> <input id="submit" type="submit" value="Do it!" />
+    <body class="body">
+        <div class="outer rounded">
+            <form action="howmany.php" method="get">
+                <h1>How many <select name="s"><option value="bing">Bings</option><option value="google">Googles</option><option value="yahoo">Yahoos</option></select> would you like?</h1>
+                <input type="text" name="x" size="2" maxlength="2" /> by <input type="text" name="y" size="2" maxlength="2" /> = <span>:(</span> <input id="submit" type="submit" class="rounded" value="Do it!" />
             </form>
-            <span id="link"></span>
+            <span id="link" class="rounded"></span>
         </div>
     </body>
 </html>
